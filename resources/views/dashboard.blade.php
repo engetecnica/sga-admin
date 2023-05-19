a2f4e6e03bffdc74a5b0b6768c0a2d90dd47dac9
Wagner Bugs <wagnerbugs@gmail.com>
2023-05-19 04:04:25 -0300

    fix: sweet/toast alert

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,13 +13,13 @@
     </title>
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
+    <link href="{{ asset('assets/images/favicon.ico') }}" rel="shortcut icon" />
 
     <!-- mdi-icons:css -->
-    <link rel="stylesheet" href="{{ asset('assets/components/css/materialdesignicons.min.css') }}">
+    <link href="{{ asset('assets/components/css/materialdesignicons.min.css') }}" rel="stylesheet">
 
     <!-- default-style:css -->
-    <link rel="stylesheet" href="{{ asset('assets/components/css/style.css') }}">
+    <link href="{{ asset('assets/components/css/style.css') }}" rel="stylesheet">
 
     <!-- bootstrap:css-4.1.3 -->
     <link href="{{ asset('assets/components/css/bootstrap.min.css') }}" rel="stylesheet" />
@@ -39,22 +39,19 @@
     <div class="container-scroller">
 
         <!-- partial:partials/_navbar.html -->
-        <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo" href="{{ env('APP_URL') }}"><img
-                        src="{{ asset('assets/images/logo-mini.png') }}" alt="{{ env('APP_NAME') }}" /></a>
-                <a class="navbar-brand brand-logo-mini" href="{{ env('APP_URL') }}"><img
-                        src="{{ asset('assets/images/logo-mini.png') }}" alt="{{ env('APP_NAME') }}" /></a>
+        <nav class="navbar default-layout-navbar col-lg-12 col-12 fixed-top d-flex flex-row p-0">
+            <div class="navbar-brand-wrapper d-flex align-items-center justify-content-center text-center">
+                <a class="navbar-brand brand-logo" href="{{ env('APP_URL') }}"><img src="{{ asset('assets/images/logo-mini.png') }}" alt="{{ env('APP_NAME') }}" /></a>
+                <a class="navbar-brand brand-logo-mini" href="{{ env('APP_URL') }}"><img src="{{ asset('assets/images/logo-mini.png') }}" alt="{{ env('APP_NAME') }}" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-stretch">
-                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                <button class="navbar-toggler navbar-toggler align-self-center" data-toggle="minimize" type="button">
                     <span class="mdi mdi-menu"></span>
                 </button>
 
                 <ul class="navbar-nav navbar-nav-right">
                     <li class="nav-item nav-profile dropdown">
-                        <a class="nav-link dropdown-toggle" id="profileDropdown" href="#"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" id="profileDropdown" data-bs-toggle="dropdown" href="#" aria-expanded="false">
                             <div class="nav-profile-img">
                                 <img src="{{ asset('assets/images/faces/face1.jpg') }}" alt="image">
                                 <span class="availability-status online"></span>
@@ -77,23 +74,19 @@
                         </a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" data-bs-toggle="dropdown" href="#" aria-expanded="false">
                             <i class="mdi mdi-email-outline"></i>
                             <span class="count-symbol bg-warning"></span>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
-                            aria-labelledby="messageDropdown">
-                            <h6 class="p-3 mb-0">Messages</h6>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
+                            <h6 class="mb-0 p-3">Messages</h6>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item preview-item">
                                 <div class="preview-thumbnail">
-                                    <img src="{{ asset('assets/images/faces/face4.jpg') }}" alt="image"
-                                        class="profile-pic">
+                                    <img class="profile-pic" src="{{ asset('assets/images/faces/face4.jpg') }}" alt="image">
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <h6 class="preview-subject ellipsis font-weight-normal mb-1">Mark send you a message
                                     </h6>
                                     <p class="text-gray mb-0"> 1 Minutes ago </p>
                                 </div>
@@ -101,12 +94,10 @@
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item preview-item">
                                 <div class="preview-thumbnail">
-                                    <img src="{{ asset('assets/images/faces/face2.jpg') }}" alt="image"
-                                        class="profile-pic">
+                                    <img class="profile-pic" src="{{ asset('assets/images/faces/face2.jpg') }}" alt="image">
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <h6 class="preview-subject ellipsis font-weight-normal mb-1">Cregh send you a
                                         message</h6>
                                     <p class="text-gray mb-0"> 15 Minutes ago </p>
                                 </div>
@@ -114,30 +105,26 @@
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item preview-item">
                                 <div class="preview-thumbnail">
-                                    <img src="{{ asset('assets/images/faces/face3.jpg') }}" alt="image"
-                                        class="profile-pic">
+                                    <img class="profile-pic" src="{{ asset('assets/images/faces/face3.jpg') }}" alt="image">
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Profile picture
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <h6 class="preview-subject ellipsis font-weight-normal mb-1">Profile picture
                                         updated
                                     </h6>
                                     <p class="text-gray mb-0"> 18 Minutes ago </p>
                                 </div>
                             </a>
                             <div class="dropdown-divider"></div>
-                            <h6 class="p-3 mb-0 text-center">4 new messages</h6>
+                            <h6 class="mb-0 p-3 text-center">4 new messages</h6>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#"
-                            data-bs-toggle="dropdown">
+                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" data-bs-toggle="dropdown" href="#">
                             <i class="mdi mdi-bell-outline"></i>
                             <span class="count-symbol bg-danger"></span>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
-                            aria-labelledby="notificationDropdown">
-                            <h6 class="p-3 mb-0">Notificações</h6>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                            <h6 class="mb-0 p-3">Notificações</h6>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item preview-item">
                                 <div class="preview-thumbnail">
@@ -145,8 +132,7 @@
                                         <i class="mdi mdi-calendar"></i>
                                     </div>
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                     <h6 class="preview-subject font-weight-normal mb-1">Event today</h6>
                                     <p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today
                                     </p>
@@ -159,8 +145,7 @@
                                         <i class="mdi mdi-settings"></i>
                                     </div>
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                     <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
                                     <p class="text-gray ellipsis mb-0"> Update dashboard </p>
                                 </div>
@@ -172,14 +157,13 @@
                                         <i class="mdi mdi-link-variant"></i>
                                     </div>
                                 </div>
-                                <div
-                                    class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                     <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
                                     <p class="text-gray ellipsis mb-0"> New admin wow! </p>
                                 </div>
                             </a>
                             <div class="dropdown-divider"></div>
-                            <h6 class="p-3 mb-0 text-center">See all notifications</h6>
+                            <h6 class="mb-0 p-3 text-center">See all notifications</h6>
                         </div>
                     </li>
                     <li class="nav-item nav-logout d-none d-lg-block">
@@ -193,8 +177,7 @@
                         </a>
                     </li>
                 </ul>
-                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
-                    data-toggle="offcanvas">
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" data-toggle="offcanvas" type="button">
                     <span class="mdi mdi-menu"></span>
                 </button>
             </div>
@@ -205,7 +188,7 @@
             <nav class="sidebar sidebar-offcanvas" id="sidebar">
                 <ul class="nav">
                     <li class="nav-item nav-profile">
-                        <a href="#" class="nav-link">
+                        <a class="nav-link" href="#">
                             <div class="nav-profile-image">
                                 <img src="{{ asset('assets/images/faces/face1.jpg') }}" alt="profile">
                                 <span class="login-status online"></span>
@@ -228,8 +211,7 @@
                         <li class="nav-item">
 
                             @if (count($module['submodulo']) > 0)
-                                <a class="nav-link" data-bs-toggle="collapse" href="#{{ $module['url_amigavel'] }}"
-                                    aria-expanded="false" aria-controls="{{ $module['url_amigavel'] }}">
+                                <a class="nav-link" data-bs-toggle="collapse" href="#{{ $module['url_amigavel'] }}" aria-expanded="false" aria-controls="{{ $module['url_amigavel'] }}">
                                     <span class="menu-title">{{ $module['titulo'] }}</span>
                                     <i class="menu-arrow"></i>
                                     <i class="{{ $module['icone'] }} menu-icon"></i>
@@ -247,11 +229,9 @@
                                         @foreach ($module['submodulo'] as $sub)
                                             <?php
                                             $item = env('URL_APP_ADMIN') . Request::segment(2) . '/' . Request::segment(3);
-                                            
+
                                             ?>
-                                            <li class="nav-item"> <a
-                                                    class="nav-link {{ $item === $sub['url_amigavel'] ? 'active-submodulo' : '' }}"
-                                                    href="{{ url($sub['url_amigavel']) }}">{{ $sub['titulo'] }}</a>
+                                            <li class="nav-item"> <a class="nav-link {{ $item === $sub['url_amigavel'] ? 'active-submodulo' : '' }}" href="{{ url($sub['url_amigavel']) }}">{{ $sub['titulo'] }}</a>
                                             </li>
                                         @endforeach
                                     </ul>
@@ -280,10 +260,8 @@
 
                 <footer class="footer">
                     <div class="container-fluid d-flex justify-content-between">
-                        <span
-                            class="text-muted d-block text-center text-sm-start d-sm-inline-block">{{ env('APP_COPY') }}</span>
-                        <span
-                            class="float-none float-sm-end mt-1 mt-sm-0 text-end">{{ env('APP_COPY_DEVELOPMENT') }}</span>
+                        <span class="text-muted d-block text-sm-start d-sm-inline-block text-center">{{ env('APP_COPY') }}</span>
+                        <span class="float-sm-end mt-sm-0 float-none mt-1 text-end">{{ env('APP_COPY_DEVELOPMENT') }}</span>
                     </div>
                 </footer>
                 <!-- partial -->
@@ -348,8 +326,11 @@
 
         $(document).ready(function() {
             $('#lista-simples').DataTable({
+                order: [
+                    [0, 'desc']
+                ],
                 language: {
-                    search: 'Buscar informação da Lista',
+                    search: 'Buscar',
                     url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json',
                 }
             });
@@ -785,13 +766,21 @@
                     alert(quantidade);
                 });
 
-        })
+        });
+
+
+        @if (Session::get('fail'))
+            $('.toastrDefaultError').ready(function() {
+                toastr.error('{{ Session::get('fail') }}')
+            });
+        @endif
+
+        @if (Session::get('success'))
+            $('.toastrDefaultSuccess').ready(function() {
+                toastr.success('{{ Session::get('success') }}')
+            });
+        @endif
     </script>
-
-
-
-
-
 
 </body>
 

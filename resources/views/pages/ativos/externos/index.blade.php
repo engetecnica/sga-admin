07b4222c9437423f7caeb5286127ac314d101f3c
Wagner Bugs <wagnerbugs@gmail.com>
2023-05-26 02:41:05 -0300

    fix: ativo externo softdelete

--- resources/views/pages/ativos/externos/index.blade.php
+++ resources/views/pages/ativos/externos/index.blade.php
@@ -2,58 +2,55 @@
 @section('title', 'Ativos Externos')
 @section('content')
 
+    <div class="page-header">
+        <h3 class="page-title">
+            <span class="page-title-icon bg-gradient-primary me-2 text-white">
+                <i class="mdi mdi-access-point-network menu-icon"></i>
+            </span> Ativos Externos
+        </h3>
+        <nav aria-label="breadcrumb">
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">
+                    <span></span>Ativos <i class="mdi mdi-check icon-sm text-primary align-middle"></i>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
-<div class="page-header">
-    <h3 class="page-title">
-        <span class="page-title-icon bg-gradient-primary text-white me-2">
-            <i class="mdi mdi-access-point-network menu-icon"></i>
-        </span> Ativos Externos
-    </h3>
-    <nav aria-label="breadcrumb">
-        <ul class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page">
-                <span></span>Ativos <i class="mdi mdi-check icon-sm text-primary align-middle"></i>
-            </li>
-        </ul>
-    </nav>
-</div>
+    <div class="page-header">
+        <h3 class="page-title">
+            <a href="{{ route('ativo.externo.adicionar') }}">
+                <button class="btn btn-sm btn-danger">Inclusão de Novos Ativos</button>
+            </a>
+        </h3>
+    </div>
 
-<div class="page-header">
-    <h3 class="page-title">
-        <a href="{{ route('ativo.externo.adicionar') }}">
-            <button class="btn btn-sm btn-danger">Inclusão de Novos Ativos</button>
-        </a>
-    </h3>
-</div>
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
 
+                    <table class="table-hover table-striped yajra-datatable table pt-4">
+                        <thead>
+                            <tr>
+                                <th width="8%">ID</th>
+                                <th>Categoria</th>
+                                <th>Título</th>
+                                <th>Data de Inclusão</th>
+                                <th>Status</th>
+                                <th width="10%">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-<div class="row">
-    <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body">
-
-                <table class="table table-hover table-striped yajra-datatable pt-4 ">
-                    <thead>
-                        <tr>
-                            <th width="8%">ID</th>
-                            <th>Categoria</th>
-                            <th>Título</th>
-                            <th>Data de Inclusão</th>
-                            <th>Status</th>
-                            <th width="10%">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
@@ -94,7 +91,9 @@
                     searchable: true
                 },
             ],
-            order: [[2, 'asc']],
+            order: [
+                [2, 'desc']
+            ],
             language: {
                 search: 'Buscar informação da Lista',
                 url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json',

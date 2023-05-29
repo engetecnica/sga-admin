8f7fff9f8df3a567915aa406fbee32629533a660
Wagner Bugs <wagnerbugs@gmail.com>
2023-05-29 02:10:55 -0300

    chore: Schedule configurado para backup note: Seguir tutorial no arquivo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,21 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+
+        //Backup agendado para ser executado as 2:00 de cada semana no Domingo.
+        $schedule->command('backup:run')
+            ->weekly()
+            ->sundays()
+            ->timezone('America/Sao_Paulo')
+            ->at('02:00');
+
+        // TUTORIAL
+        // Para rodar o comando corretamente, acesse o servidor de hospedagem, geralmente em SSH.
+        // 1) Acesse até a pasta do projeto e digite o comando "pwd" para pegar o path do projeto. ANOTE: /caminho/do/projeto
+        // 2) Digite o comando "crontab -e" e escolha o NANO para editar o arquivo. [-e de edit]
+        // 3) É só digitar: "* * * * * cd /caminho/para/o/projeto && php artisan schedule:run >> /dev/null 2>&1" [sem aspas]. CTRL + 0 para salvar e CTRL + X para sair e Y para confirmar.
+        // 4) Para ver o cron job (lista de tarefas agendadas), digite o comando "crontab -l" e será exibida a lista de tarefas. [-l de list]
+
     }
 
     /**
